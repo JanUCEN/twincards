d31480e59e0b14d991e0fee599bd53dfeb5b9f7e
Jan Shah <shahj@ucenmanchester.ac.uk>
2025-03-13 15:12:45 +0000

    check card logic

--- src/main/java/level5/software/App.java
+++ src/main/java/level5/software/App.java
@@ -18,7 +18,8 @@ import java.util.List;
 public class App extends Application {
     private GameBoard board;
     private List<Button> cards = new ArrayList<>();
-
+    Card flipped1 = null;
+    Card flipped2 = null;
 
     private static Scene scene;
 
@@ -34,18 +35,18 @@ public class App extends Application {
         int cardWidth = 50;
         int cardHeight = 50;
         board = new GameBoard(pairs);
-        List<Button> cards = new ArrayList<>();
+        cards = new ArrayList<>();
 
         for (Card card : board.getCards()) {
             Button btn = new Button("?");
             btn.setMinWidth(cardWidth - 2);
             btn.setMinHeight(cardHeight - 2);
             cards.add(btn);
-
+            
             int index = card.getIndex();
-
+            
+            btn.setId(Integer.toString(index));
             btn.setOnAction(e -> checkCard(card, btn));
-
             grid.add(btn, index % cols, index / cols);
         }
 
@@ -55,8 +56,33 @@ public class App extends Application {
     }
 
     private void checkCard(Card card, Button btn) {
-        System.out.println(card.getValue());
-        btn.setText(card.getValue());
+        if(card.isFlipped()) {
+            return;
+        }
+        card.flip();
+
+        if(flipped1 == null) {
+            btn.setText(card.getValue());
+            flipped1 = card;
+            return;
+        }
+        if(flipped2 == null) {
+            btn.setText(card.getValue());
+            flipped2 = card;
+        }
+        System.out.println(flipped1.getValue());
+        System.out.println(flipped2.getValue());
+        if(flipped1.getValue().equals(flipped2.getValue())) {
+            flipped1 = null;
+            flipped2 = null;
+            return;
+        } 
+        flipped1.flip();
+        flipped2.flip();
+        btn.setText("?");
+        cards.get(flipped1.getIndex()).setText("?");
+        flipped1 = null;
+        flipped2 = null;
     }
 
     static void setRoot(String fxml) throws IOException {
